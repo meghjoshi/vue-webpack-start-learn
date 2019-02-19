9880e7a1ad06f5860aaa6a2566cc587c59f722ca
meghjoshi <meghna@blavity.com>
2019-02-19 14:44:01 +0530

    change the api path

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   AppName: '"Blavity"',
-  LiveAPI: '"https://api.blavity.com/v1/"',
+  LiveAPI: '"https://www.api.blavity.com/v1/"',
   LocalAPI: '"http://192.168.200.25:3100/v1/"',
   cloudinaryImageUrl: '"https://res.cloudinary.com/blavity/image/upload/c_crop,g_center,w_auto,q_auto:best,g_south_east,x_0/"',
 })
